3a18d8a1e8538138f908a14cf215f57cd531b4f1
alyson-betz <alyson.fantastic@gmail.com>
2021-04-25 12:04:14 -0500

    Lighting for flashlight
    Basic lighting for the flashlight

---
+++ LudumDareApril2021/Assets/Lighting Scripts/RotateWithMouse.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWithMouse : MonoBehaviour
+{
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookTarget = Vector3.zero;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            lookTarget = hit.point;
+        }
+        transform.LookAt(lookTarget);
+    }
+}
